1ba00f14f87bd2ffc6da882d8d9a65e89e568e68
Emma Darbois <73026258+emmadarbois@users.noreply.github.com>
2024-07-08 15:36:20 +0200

    update homepage

--- src/components/homePageWindow/homePageWindow.js
+++ src/components/homePageWindow/homePageWindow.js
@@ -2,13 +2,19 @@ import "../../variables.css"
 import "./homePageWindow.css"
 import Button from "react-bootstrap/Button"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons/faXmark";
 
 export default function HomePageWindow() {
+    function handleClick() {
+        alert('You clicked me!');
+    }
+
     return (
 
       <div className="outer-border">
         <div className="upper-line">
-            <div className="cross"></div>
+                <FontAwesomeIcon icon={faXmark} className="x-mark-icon" onClick={handleClick} />
         </div>
 
         <div className="window-main-body">
